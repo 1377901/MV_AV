72a22a25078b578fe07162d9e2845431860a22c6
1377901 <87511557+1377901@users.noreply.github.com>
2021-07-26 16:21:31 +0800

    研究元组数据

--- face_recognition_2.py
+++ face_recognition_2.py
@@ -35,14 +35,27 @@ while(True):
     dist = 0
     d1 = img.find_lbp((0, 0, img.width(), img.height()))
     dist = image.match_descriptor(d0, d1) #计算d0 d1即样本图像与被检测人脸的特征差异度。
-    if (dist < 100): #识别到目标
-        objects = img.find_features(face_cascade, threshold=0.95, scale_factor=1.25)
-        # Draw objects
-        for r in objects:
-            img.draw_rectangle(r, color = (255, 0, 0))
-            img.draw_cross(r.cx(), r.cy(), color = (0, 255, 0))
-            send_frame(r.cx(), r.cy())
-            print("Object x:%d: y:%d, The dis: %f"%(r.cx(), r.cy(),dist))
+    # if (dist < 100): #识别到目标
+    objects = img.find_features(face_cascade, threshold=0.95, scale_factor=1.25)
+    # Draw objects
+    for r in objects:
+        temptang0 = img.draw_rectangle(r, color = (255, 0, 0))
+        #print(temptang0) # 只有w h 没有x y
+        #print(r) # x y w h
+        print(r[0],r[1],r[2],r[3])
+        print(type(r))
+
+
+        #img = sensor.snapshot().gamma_corr(contrast=1.5) #拍摄当前图像
+        #temptangs = img.find_rects()
+
+        #temp_x = temptang.x() + temptang.w()/2
+        #temp_y = temptang.y() + temptang.h()/2
+        ## temp_x = r.cx()
+        ## temp_y = r.cy()
+        #img.draw_cross(temp_x, temp_y, color = (0, 255, 0),size=10)
+        #send_frame(temp_x, temp_y)
+        #print("Object x:%d: y:%d, The dis: %f"%(temp_x, temp_y,dist))
 
     #print(clock.fps())
 
