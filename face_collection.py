138751eb9980ba7024c49df1075789faf10b40bd
1377901 <87511557+1377901@users.noreply.github.com>
2021-07-26 16:52:48 +0800

    修正4
    不使用TF搜集人脸

--- face_collection.py
+++ face_collection.py
@@ -7,7 +7,8 @@ SUB = "original"  #目标图像所在目录
 
 sensor.reset() # Initialize the camera sensor.
 sensor.set_pixformat(sensor.GRAYSCALE) # or sensor.GRAYSCALE
-sensor.set_framesize(sensor.B128X128) # or sensor.QQVGA (or others)
+# sensor.set_framesize(sensor.B128X128) # or sensor.QQVGA (or others)
+sensor.set_framesize(sensor.QVGA)
 sensor.set_windowing((92,112))
 sensor.skip_frames(10) # Let new settings take affect.
 sensor.skip_frames(time = 2000)
--- face_recognition_2.py
+++ face_recognition_2.py
@@ -3,7 +3,8 @@ import sensor, time, image, pyb
 
 sensor.reset() # Initialize the camera sensor.
 sensor.set_pixformat(sensor.GRAYSCALE) # or sensor.GRAYSCALE
-sensor.set_framesize(sensor.B128X128) # or sensor.QQVGA (or others)
+# sensor.set_framesize(sensor.B128X128) # or sensor.QQVGA (or others)
+sensor.set_framesize(sensor.QVGA)
 sensor.set_windowing((92,112))
 sensor.skip_frames(10) # Let new settings take affect.
 sensor.skip_frames(time = 5000) #等待5s
@@ -20,7 +21,7 @@ def send_frame(cx, cy):
      uart.writechar(cx + cy)
 
 
-img = image.Image("../original/1.bmp").mask_ellipse() #目标图像所在目录
+img = image.Image("../original/1.pgm").mask_ellipse() #目标图像所在目录
 d0 = img.find_lbp((0, 0, img.width(), img.height()))  #d0为当前人脸的lbp特征
 img = None
 face_cascade = image.HaarCascade("frontalface", stages=25)
@@ -31,7 +32,7 @@ clock = time.clock()
 while(True):
     clock.tick()
     # img = sensor.snapshot()
-    img = sensor.snapshot().gamma_corr(contrast=1.5) #拍摄当前图像
+    img = sensor.snapshot().gamma_corr(contrast=2) #拍摄当前图像
     dist = 0
     d1 = img.find_lbp((0, 0, img.width(), img.height()))
     dist = image.match_descriptor(d0, d1) #计算d0 d1即样本图像与被检测人脸的特征差异度。
@@ -40,11 +41,6 @@ while(True):
     # Draw objects
     for r in objects:
         temptang0 = img.draw_rectangle(r, color = (255, 0, 0))
-        #print(temptang0) # 只有w h 没有x y
-        #print(r) # x y w h
-        # print(r[0],r[1],r[2],r[3])
-        # print(type(r))
-
         temp_x = int(r[0] + r[2]/2)
         temp_y = int(r[1] + r[3]/2)
         img.draw_cross(temp_x, temp_y, color = (0, 255, 0),size=10)
