1933dab1fe634afb3edb805e8dd9b61e49ceb87e
1377901 <87511557+1377901@users.noreply.github.com>
2021-09-07 16:29:16 +0800

    修改：只识别

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ import image, math, pyb, sensor, struct, time
 ##配置输出
 uart_baudrate = 115200
 uart = pyb.UART(3, uart_baudrate, timeout_char = 1000)
-def send_frame(cx, cy, flag, time, lat, lon):
+def send_frame(cx, cy, flag):
      uart.writechar(0xA5)
      uart.writechar(0x5A)
      uart.writechar(cx)
      uart.writechar(cy)
      uart.writechar(cx + cy) #校验位
      uart.writechar(flag)
-     uart.writechar(time)
-     uart.writechar(lat)
-     uart.writechar(lon)
+
 
 sensor.reset()
 sensor.set_pixformat(sensor.GRAYSCALE)
@@ -42,9 +40,6 @@ v_fov = 2 * math.atan((sensor_h_mm / 2) / lens_mm)
 ##功能函数
 clock = time.clock()
 tag_flag = 0  ##指定该点位的有效性 0:表示无效 1：表示有效
-loiter_time = 0 ##指定无人机在TAG处悬停时间
-position_lat = None
-position_lon = None
 
 while(True):
     clock.tick()
@@ -55,28 +50,16 @@ while(True):
         img.draw_cross(tags[0].cx(), tags[0].cy(), color = (0, 255, 0), size=10)
         if tags.id() == 0: ##TAG 0 代表降落点--禁止修改
             tag_flag = 1
-            loiter_time = 0
-            position_lat = 0
-            position_lon = 0
-            send_frame(tags[0].cx(), tags[0].cy(), tag_flag, loiter_time, position_lat, position_lon)
+            send_frame(tags[0].cx(), tags[0].cy(), tag_flag)
         if tags.id() == 1: ##假设TAG 1 代表香蕉
-            tag_flag = 1 #启用该点
-            loiter_time = 5 ##指定该点的悬停时间--秒
-            position_lat = None ##指定该TAG投放点的纬度
-            position_lon = None ##指定该TAG投放点的经度
-            send_frame(tags[0].cx(), tags[0].cy(), tag_flag, loiter_time, position_lat, position_lon)
+            tag_flag = 0
+            send_frame(tags[0].cx(), tags[0].cy(), tag_flag)
         if tags.id() == 2: ##假设TAG 2 代表苹果
-            tag_flag = 1 #启用该点
-            loiter_time = 5 ##指定该点的悬停时间--秒
-            position_lat = None ##指定该TAG投放点的纬度
-            position_lon = None ##指定该TAG投放点的经度
-            send_frame(tags[0].cx(), tags[0].cy(), tag_flag, loiter_time, position_lat, position_lon)
+            tag_flag = 0 
+            send_frame(tags[0].cx(), tags[0].cy(), tag_flag)
         if tags.id() == 3: ##假设TAG 3 代表橘子
-            tag_flag = 1 #启用该点
-            loiter_time = 5 ##指定该点的悬停时间--秒
-            position_lat = None ##指定该TAG投放点的纬度
-            position_lon = None ##指定该TAG投放点的经度
-            send_frame(tags[0].cx(), tags[0].cy(), tag_flag, loiter_time, position_lat, position_lon)
+            tag_flag = 0 
+            send_frame(tags[0].cx(), tags[0].cy(), tag_flag)
 
     img = 0
     tags = 0
