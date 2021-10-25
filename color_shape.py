39b73b5a487400399552d8a32c3dd22a2489fcf8
1377901 <87511557+1377901@users.noreply.github.com>
2021-10-25 11:29:44 +0800

    定2

--- color_shape.py
+++ color_shape.py
@@ -8,41 +8,21 @@ sensor.set_auto_gain(False) # must be turned off for color tracking
 sensor.set_auto_whitebal(False) # must be turned off for color tracking
 clock = time.clock()
 
-# thresholds = [(0, 0, -1, 2, -1, 1)]
-black = [(0, 0, -1, 2, -1, 1)]
-white = [(80, 100, -1, 100, -1, 100)]
-shre = [(41, 100, -1, 11, -32, 85)]
-
 while(True):
     #time.sleep(0.5)
     clock.tick()
     img = sensor.snapshot().lens_corr(1.8)
     for r in img.find_rects(threshold = 10000):
-    # for r in img.find_rects(thresholds):
 
         area = r.rect()
-        #print(r.w(),r.h())
         size = r.magnitude() #矩形大小
-        if(15000 < size < 20000):
+        if(15000 < size < 25000):
             img.draw_rectangle(r.rect(), color = (255, 0, 0))
             print(r.w(),r.h())
-            #for p in r.corners(): img.draw_circle(p[0], p[1], 5, color = (0, 255, 0))
-        if(size > 20000):
-            for p in r.corners(): img.draw_circle(p[0], p[1], 5, color = (0, 255, 0))
-
-        statistics = img.get_statistics(roi=area)#像素颜色统计
-        #if 80<statistics.l_mode()<100 and -1<statistics.a_mode()<1 and -1<statistics.b_mode()<1:
-            #for p in r.corners(): img.draw_circle(p[0], p[1], 5, color = (0, 255, 0))
-
-        # print(r)
-    #black_blobs = img.find_blobs(white)
-    pixels_threshold = 20000
-    black_blobs = img.find_blobs(shre,pixels_threshold)
-    for b in black_blobs:
-        if 50000 > b.area() > 500:
-            #print(b.cx())
-            img.draw_rectangle(b.rect(), color = (0, 0, 255))
-            print(b.w(),b.h())
+        if(size > 25000):
+            for p in r.corners():
+                img.draw_circle(p[0], p[1], 5, color = (0, 255, 0))
 
 
-    #print("FPS %f" % clock.fps())
+
+
