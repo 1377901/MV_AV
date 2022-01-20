14588fa0c673c20a1f85f8b888a0b2f93a18f133
1377901 <87511557+1377901@users.noreply.github.com>
2022-01-20 15:59:49 +0800

    修1
    查找

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ sensor.set_framesize(sensor.QVGA)
 sensor.set_auto_gain(True)
 sensor.set_auto_whitebal(True) #
 #sensor.set_pixformat(sensor.GRAYSCALE)
-sensor.skip_frames(time = 90)
+sensor.skip_frames(time = 2000)
 clock = time.clock()
 
 #threshold2 = [(79, 100, -128, 127, -128, 127)]
 #threshold2 = [(89, 100, -128, 127, -128, 127)] #(89, 100, -17, 6, -9, 23)
-threshold2 = [(93, 100, -128, 127, -127, 127)]
+threshold2 = [(63, 100, -128, 127, -127, 127)]
 threshold_index = 0
 blob_t = 0
 
@@ -32,23 +32,29 @@ while(True):
     #img = sensor.snapshot().lens_corr(1.8)
     #img = sensor.snapshot().lens_corr(0.5)
     img = sensor.snapshot()
-    for blob in img.find_blobs([threshold2[threshold_index]], pixels_threshold=100, area_threshold=100, merge=True):
+    blobs = img.find_blobs([threshold2[threshold_index]], pixels_threshold=1000, area_threshold=1000, merge=True)
+    blob_count = 0
+    for blob in blobs:
         blob_t = 1
+        blob_count = blob_count + 1
+        #img.draw_rectangle(blob.rect(),color=(0,255,0))
         pass
+    #print(blobs[1])
 
-    for r in img.find_rects(threshold = 5000):
+    temp_data = img.find_rects(threshold = 1000)
+    for r in temp_data:
         size = r.magnitude() #矩形大小
+        #img.draw_rectangle(blob.rect(),color=(0,255,0))
+        #img.draw_cross(blob.cx(), blob.cy(),color=(255,0,0))
 
-        if(500000 > size > 5000)and(blob_t == 1):
-            img.draw_rectangle(blob.rect())
+        if(10000 > size > 5000)and(blob_t == 1):
+            img.draw_rectangle(blob.rect(),color=(0,255,0))
             img.draw_cross(blob.cx(), blob.cy(),color=(255,0,0))
-            if(blob.cx() > 0):
-                send_frame(blob.cx(), blob.cy(), 1)
-                # print(blob.cx(),blob.cy())
-                deltax = blob.cx() - 160
-                deltay = blob.cy() - 120
-                dis = math.sqrt(deltax*deltax + deltay*deltay)
-                print(blob.cx(),blob.cy(),dis)
+            send_frame(blob.cx(), blob.cy(), 1)
+            deltax = blob.cx() - 160
+            deltay = blob.cy() - 120
+            dis = math.sqrt(deltax*deltax + deltay*deltay)
+            print(blob.cx(),blob.cy(),dis,size)
 
 
 
